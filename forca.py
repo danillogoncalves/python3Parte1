662a67a8a497d7dcffb836fa45d232deb79fda42
Danillo <danillo.ooo@hotmail.com>
2021-06-16 20:35:44 -0300

    Conteúdo da aulas de Python 3 parte 1

---
+++ forca.py
@@ -0,0 +1,12 @@
+
+def jogar():
+
+    print('*********************************')
+    print('***Bem vindo ao jogo de Forca!***')
+    print('*********************************')
+
+    print('Fim do jogo')
+
+if(__name__ == '__main__'):
+    jogar()
+
