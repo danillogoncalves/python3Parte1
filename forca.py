da4786176a26a53e405a98442ed967dd86dd564f
Danillo <danillo.ooo@hotmail.com>
2021-06-17 23:14:43 -0300

    Iniciamos o jogo da forca no curso Python 3 parte 2, estamos iniciando o primeiro loop de verificação no jogo, para testar se o jogador se enforcou ou ainda não acertou

--- forca.py
+++ forca.py
@@ -1,12 +1,21 @@
-
 def jogar():
-
     print('*********************************')
     print('***Bem vindo ao jogo de Forca!***')
     print('*********************************')
+
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acerto = False
+
+    #enquanto não enforcou E não acertou - o not negada o valor bool(booleano)
+    #enquanto(not False and not False):
+    #enquanto(True and True):
+    #enquanto(True):
+    while(not enforcou and not acerto):
+        print('Jogando...')
 
     print('Fim do jogo')
 
 if(__name__ == '__main__'):
     jogar()
-
