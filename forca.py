7613ac6eb3c9e23baff1b603d76076d800083eb9
Danillo <danillo.ooo@hotmail.com>
2021-06-30 20:56:40 -0300

    Aula 06 Escrita e leitura de arquivo – Aprendemos a criar/escrever um arquivo, e no novo jogo da forca usamos isso para ler um arquivo para gerar randomicamente as strings do arquivo.

--- forca.py
+++ forca.py
@@ -1,13 +1,26 @@
+import random
+
 def jogar():
     print('*********************************')
     print('***Bem vindo ao jogo de Forca!***')
     print('*********************************')
 
-    palavra_secreta = 'maça'.upper()
-    letras_acertadas = ['_' for letra in palavra_secreta]
+    # arquivo = open('paravras.txt', 'r')
+    # Foi mudado para não termos que usa o .close() no final, da fora abaixo ele fecha o arquivo no final da função.
+    # Tive que acrecentar o encoging, pois isso não estava sendo feito como padrão o UTF-8, estava o cp1252.
+    with open('palavras.txt', 'r', encoding='UTF-8') as arquivo:
+        palavras = []
+
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
 
 #   for letra in palavra_secreta:
 #       letras_acertadas.append('_')
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acerto = False
